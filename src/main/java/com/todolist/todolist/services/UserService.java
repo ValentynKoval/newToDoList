aacc7dffc1fd2ea46ef21dfa6014e4bb32821b84
Valentyn <J.K.play2015@gmail.com>
2024-10-20 16:05:44 +0300

    create user service

---
+++ src/main/java/com/todolist/todolist/services/UserService.java
@@ -0,0 +1,41 @@
+package com.todolist.todolist.services;
+
+import com.todolist.todolist.models.User;
+import com.todolist.todolist.models.enums.Role;
+import com.todolist.todolist.repo.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(String.format("User %s not found", username)));
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                user.getRoles());
+    }
+
+    public void createNewUser(User user) {
+        user.setRoles(Set.of(Role.ROLE_USER));
+        userRepository.save(user);
+    }
+}
